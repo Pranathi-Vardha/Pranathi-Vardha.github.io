363b0c6fe9ce0006febeedead450da347cdc4a57
Pranathi-Vardha <86348116+Pranathi-Vardha@users.noreply.github.com>
2023-06-05 21:55:20 +0530

    .

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Hi.This is an alert")
+var data1={image : "picture.jpg",name : "Vardha Pranathi", designation:"Intern"};
+var data2={image : "picture2.jpg",name : "Nimmu", designation:"Software Devoloper"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
