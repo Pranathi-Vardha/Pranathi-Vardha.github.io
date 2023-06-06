6b90911f7e1b103641944bee58bb68320fef2bd7
Pranathi-Vardha <86348116+Pranathi-Vardha@users.noreply.github.com>
2023-06-06 22:02:50 +0530

    changes in html3

--- script2.js
+++ script2.js
@@ -1,11 +1,12 @@
+var userInfo = {name : "", image : "", username : ""};
 var chnagemypic = function(){
 
-    fetch("https://randomuser.me/api").then((data)=>data.json()).then(data)=>{}
+    fetch("https://randomuser.me/api").then((response)=>response.json()).then(data)=>{
     userinfo.name = data.results[0].name.first;
     userinfo.image = data.results[0].picture.large;
     userinfo.username =data.results[0].login.username;
-    document.getElementByid("myimage") = src.userinfo.image;
-    document.getElementByid("myname").innerHTML = src.userinfo.name;
-    document.getElementByid("mydesig").innerHTML = src.userinfo.username;
-
+    document.getElementByid("myimage").src =userinfo.image;
+    document.getElementByid("myname").innerHTML = userinfo.name;
+    document.getElementByid("mydesig").innerHTML = userinfo.username;
+    });
 }
