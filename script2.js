b22af81bc7ff437b67414534b26a8b03ee171af0
Pranathi-Vardha <86348116+Pranathi-Vardha@users.noreply.github.com>
2023-06-06 22:21:11 +0530

    .

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
 var userInfo = {name : "", image : "", username : ""};
 var chnagemypic = function(){
 
-    fetch("https://randomuser.me/api").then((response)=>response.json()).then(data)=>{
+    fetch("https://randomuser.me/api").then((response)=>response.json()).then((data)=>{
     userinfo.name = data.results[0].name.first;
     userinfo.image = data.results[0].picture.large;
     userinfo.username =data.results[0].login.username;
     document.getElementByid("myimage").src =userinfo.image;
     document.getElementByid("myname").innerHTML = userinfo.name;
-    document.getElementByid("mydesig").innerHTML = userinfo.username;
+    document.getElementByid("mydesig").src = userinfo.username;
     });
 }
